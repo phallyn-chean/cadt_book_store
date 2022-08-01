730496af09cc5487206eb4da4ecd0cc7f3e43e1d
phallyn-chean <phollyn.chean@student.cadt.edu.kh>
2022-08-01 17:05:28 +0700

    update booklist.dart

--- lib/ui/screen/bookList.dart
+++ lib/ui/screen/bookList.dart
@@ -21,101 +21,125 @@ class BookList extends StatelessWidget {
           name,style: TextStyle(fontSize: 25),
         ),
       ),
-      // body: Consumer(
-      //   builder: (context, value, child){
-      //     return FutureBuilder(
-      //       builder: (context, AsyncSnapshot snapshot) {
-      //         if (snapshot.hasError){
-      //           return Center(
-      //             child: Text("Opps! Try Again later!"),
-      //           );
-      //         }
-      //         if (snapshot.hasData){
-      //
-      //           return GridView.builder(
-      //             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //               crossAxisCount: 2,
-      //               mainAxisExtent: 260,
-      //               crossAxisSpacing: 0,
-      //               mainAxisSpacing: 0,
-      //             ),
-      //             itemBuilder: (context, index){
-      //               return Padding(
-      //                 padding: EdgeInsets.all(16),
-      //                 child: GestureDetector(
-      //                   onTap: () {},
-      //                   child: Container(
-      //                     height: 250,
-      //                     decoration: BoxDecoration(
-      //                       borderRadius: BorderRadius.circular(12)
-      //                     ),
-      //                     child: LayoutBuilder(
-      //                       builder: (context, constraints){
-      //                         return Column(
-      //                           crossAxisAlignment: CrossAxisAlignment.start,
-      //                           children: [
-      //                             SizedBox(
-      //                               height: constraints.maxHeight / 2,
-      //                               child: Stack(
-      //                                 alignment: Alignment.topCenter,
-      //                                 children: [
-      //                                   Container(
-      //                                     height: constraints.maxHeight / 2.5,
-      //                                     decoration: BoxDecoration(
-      //                                       //color: boxColors[_random.nextInt(7)],
-      //                                       borderRadius: BorderRadius.circular(12),
-      //                                     ),
-      //                                   ),
-      //                                   Positioned(
-      //                                     top: 0,
-      //                                     child: Card(
-      //                                       margin: EdgeInsets.zero,
-      //                                       shape: RoundedRectangleBorder(
-      //                                         borderRadius: BorderRadius.circular(12),
-      //                                       ),
-      //                                       child: SizedBox(
-      //                                         child: ClipRRect(
-      //                                           borderRadius: BorderRadius.circular(12),
-      //                                           child: Image(
-      //                                             height: constraints.maxHeight / 2,
-      //                                             width: constraints.maxWidth / 2,
-      //                                             image: NetworkImage(
-      //                                               ""
-      //                                             ),fit: BoxFit.fill,
-      //                                           )
-      //                                         ),
-      //                                       ),
-      //                                     ),
-      //                                   ),
-      //                                 ],
-      //                               ),
-      //                             ),
-      //                             Padding(
-      //                               padding: EdgeInsets.all(5.0),
-      //                               child: Column(
-      //                                 crossAxisAlignment: CrossAxisAlignment.start,
-      //                                 children: [
-      //                                   Text("",
-      //                                     maxLines: 1,
-      //                                     style: Theme.of(context).textTheme.headline4 ?.copyWith(fontSize: constraints.maxWidth * 0.09),
-      //                                   ),
-      //                                 ],
-      //                               ),
-      //                             )
-      //                           ],
-      //                         );
-      //                       },
-      //                     ),
-      //                   ),
-      //                 ),
-      //               );
-      //             },
-      //           );
-      //         }
-      //       },
-      //     );
-      //   },
-      // )
+      body: Consumer(
+        builder: (context, value, child){
+          return FutureBuilder(
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasError){
+                return Center(
+                  child: Text("Opps! Try Again later!"),
+                );
+              }
+              if (snapshot.hasData){
+
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisExtent: 260,
+                    crossAxisSpacing: 0,
+                    mainAxisSpacing: 0,
+                  ),
+                  itemBuilder: (context, index){
+                    return Padding(
+                      padding: EdgeInsets.all(16),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: 250,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                          child: LayoutBuilder(
+                            builder: (context, constraints){
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: constraints.maxHeight / 2,
+                                    child: Stack(
+                                      alignment: Alignment.topCenter,
+                                      children: [
+                                        Container(
+                                          height: constraints.maxHeight / 2.5,
+                                          decoration: BoxDecoration(
+                                            //color: boxColors[_random.nextInt(7)],
+                                            borderRadius: BorderRadius.circular(12),
+                                          ),
+                                        ),
+                                        Positioned(
+                                          top: 0,
+                                          child: Card(
+                                            margin: EdgeInsets.zero,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(12),
+                                            ),
+                                            child: SizedBox(
+                                              child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(12),
+                                                child: Image(
+                                                  height: constraints.maxHeight / 2,
+                                                  width: constraints.maxWidth / 2,
+                                                  image: NetworkImage(
+                                                    ""
+                                                  ),fit: BoxFit.fill,
+                                                )
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(5.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("",
+                                          maxLines: 1,
+                                          style: Theme.of(context).textTheme.headline4 ?.copyWith(fontSize: constraints.maxWidth * 0.09),
+                                        ),
+                                        Text(
+                                          "",
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Theme.of(context).textTheme.headline4 ?. copyWith(fontSize: constraints.maxWidth * 0.09, fontWeight: FontWeight.bold),
+                                        ),
+                                        Container(
+                                          height: constraints.maxHeight * 0.13,
+                                          width: constraints.maxWidth * 0.35,
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(
+                                            color: Colors.black,
+                                            borderRadius: BorderRadius.circular(12),
+                                          ),
+                                          child: Text(
+                                            "",
+                                            style: TextStyle(fontSize: constraints.maxWidth * 0.08, color: Colors.white),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+              );
+            },
+          );
+        },
+      )
     );
   }
 }
